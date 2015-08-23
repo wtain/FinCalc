b88e200c0eb1c0e767dbc50425ea940a1bc42a8e
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2015-08-23 16:06:40 +0300

    Code cleanup

--- trunk/Calendar/Calendar/Calendar.cpp
+++ trunk/Calendar/Calendar/Calendar.cpp
@@ -24,36 +24,23 @@ public:
 
         BOOST_FOREACH(boost::property_tree::ptree::value_type &v, pt.get_child("data"))
         {
-            //std::cout << v.first << std::endl;
             const int year = boost::lexical_cast<int>(v.first);
-            //const std::string year = v.first;
-            //std::cout << v.second.data() << std::endl;
             BOOST_FOREACH(boost::property_tree::ptree::value_type &v1, v.second)
             {
-                //std::cout << "\t\t" << v1.first << std::endl;
-                //const std::string month = v1.first;
                 const int month = boost::lexical_cast<int>(v1.first);
                 BOOST_FOREACH(boost::property_tree::ptree::value_type &v2, v1.second)
                 {
-                    //std::cout << "\t\t\t\t" << v2.first << std::endl;
-                    //const std::string day = v1.first;
                     const int day = boost::lexical_cast<int>(v1.first);
                     boost::locale::date_time t = boost::locale::period::year(year) + 
                                                  boost::locale::period::month(month) + 
                                                  boost::locale::period::day(day);
-                    //std::ostringstream stream;
-
-                    //stream << boost::locale::as::date << t;
-                                                           
-                    //m_holidays.insert(stream.str());
+                    
                     m_holidays.insert(t);
                 }
             }
         }
     }
 };
-
-//const std::string HolidayCalendar::Format_Date_ISO = "%Y%m%d";
 
 int main(int argc, char* argv[])
 {
